c26b2baba9e9bd968b9511a276698151f2bc8748
Paulo Schneider <paulo.schneider@gmail.com>
2013-08-05 12:21:36 +0100

    Convert HTML table into d3 format treemap

--- public/javascripts/table-to-tree-map.js
+++ public/javascripts/table-to-tree-map.js
@@ -1,3 +1,11 @@
 function extractD3TreeMap(selection) {
-  return { name: "Service Explorer" }
+  return { 
+    name: "Service Explorer", 
+    children: selection[0].map(function (row) {
+      return { 
+        name: row.getAttribute("data-title"),
+        size: parseInt(row.getAttribute("data-volume"))
+      };
+    })
+  };
 }
